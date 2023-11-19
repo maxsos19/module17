3a61fc1c0291e1f0b5a92b57e9e9d04829077063
maxsos19 <maksimnkalinin@mail.ru>
2023-11-19 19:45:01 +0300

    first

---
+++ itogi_module_17/Account.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace itogi_module_17
+{
+    public abstract class Account
+    {
+        public string Type { get; set; }
+        public double Balance { get; set; }
+        public abstract double CalculateInterest();
+        public double Interest { get; set; }
+    }
+   
+}
---
+++ itogi_module_17/Calculator.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace itogi_module_17
+{
+    public static class Calculator
+    {
+        // Методы для расчета процентной ставки
+
+        public class OrdinaryAccount : Account
+        {
+            public override double CalculateInterest()
+            {
+                double interest = Balance * 0.4;
+
+                if (Balance < 1000)
+                    interest -= Balance * 0.2;
+
+                if (Balance >= 1000)
+                    interest -= Balance * 0.4;
+
+                return interest;
+            }
+            public class SalaryAccount : Account
+            {
+                public override double CalculateInterest()
+                {
+                    return Balance * 0.5;
+                }
+            }
+            public static class Calculator
+            {
+                public static void CalculateInterest(Account account)
+                {
+                    account.Interest = account.CalculateInterest();
+                }
+            }
+        }
+
+
+    }
+}
+
+            
+
+           
